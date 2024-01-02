59117e20d2880e215623f772bb71df48086214f1
jlowell000 <jlowell000@gmail.com>
2024-01-02 12:34:47 -0500

    improve coverage

--- internal/model/periodic_flow_test.go
+++ internal/model/periodic_flow_test.go
@@ -11,17 +11,16 @@ import (
 )
 
 const (
-	TEST_PF_ID      = "16cfd708-db6d-42fd-8ad1-55316690520c"
-	TEST_PF_NAME    = "test name"
-	TEST_PF_AMOUNT  = 100.99
-	TEST_PF_TIME    = "2006-01-23T15:04:05Z"
-	TEST_PF_CONTENT = "test content"
+	TEST_PF_ID     = "16cfd708-db6d-42fd-8ad1-55316690520c"
+	TEST_PF_NAME   = "test name"
+	TEST_PF_AMOUNT = 100.99
+	TEST_PF_TIME   = "2006-01-23T15:04:05Z"
 )
 
 func TestPeriodicFlowToJSON(t *testing.T) {
 	id, timestamp := getParsedValues()
 	for _, p := range Periods {
-		expected := getTestJson(p)
+		expected := getTestJson(TEST_PF_ID, TEST_PF_NAME, TEST_PF_AMOUNT, p, TEST_PF_AMOUNT, TEST_PF_TIME)
 		periodicFlow := PeriodicFlow{
 			Id:               id,
 			Name:             TEST_PF_NAME,
@@ -48,7 +47,28 @@ func TestPeriodicFlowFromJSON_data_there(t *testing.T) {
 			WeeklyAmount:     TEST_PF_AMOUNT,
 			UpdatedTimestamp: timestamp,
 		}
-		actual := PeriodicFlowFromJSON([]byte(getTestJson(p)))
+		actual := PeriodicFlowFromJSON([]byte(
+			getTestJson(TEST_PF_ID, TEST_PF_NAME, TEST_PF_AMOUNT, p, TEST_PF_AMOUNT, TEST_PF_TIME),
+		))
+
+		assert.Equal(t, expected, actual)
+	}
+}
+
+func TestPeriodicFlowFromJSON_partial_data_there(t *testing.T) {
+	id, timestamp := getParsedValues()
+
+	for _, p := range Periods {
+		expected := PeriodicFlow{
+			Id:               id,
+			Amount:           TEST_PF_AMOUNT,
+			Period:           p,
+			WeeklyAmount:     TEST_PF_AMOUNT,
+			UpdatedTimestamp: timestamp,
+		}
+		actual := PeriodicFlowFromJSON([]byte(
+			getTestJson(TEST_PF_ID, "", TEST_PF_AMOUNT, p, TEST_PF_AMOUNT, TEST_PF_TIME),
+		))
 
 		assert.Equal(t, expected, actual)
 	}
@@ -73,11 +93,18 @@ func getPFParsedValues() (uuid.UUID, time.Time) {
 	return id, timestamp
 }
 
-func getTestJson(p Period) string {
-	return "{\"id\":\"" + TEST_PF_ID +
-		"\",\"name\":\"" + TEST_PF_NAME +
-		"\",\"amount\":" + fmt.Sprintf("%.2f", TEST_PF_AMOUNT) +
+func getTestJson(
+	id string,
+	name string,
+	amount float64,
+	p Period,
+	weeklyAmount float64,
+	time string,
+) string {
+	return "{\"id\":\"" + id +
+		"\",\"name\":\"" + name +
+		"\",\"amount\":" + fmt.Sprintf("%.2f", amount) +
 		",\"period\":\"" + p.String() +
-		"\",\"weekly_amount\":" + fmt.Sprintf("%.2f", TEST_PF_AMOUNT) +
-		",\"updated_timestamp\":\"" + TEST_PF_TIME + "\"}"
+		"\",\"weekly_amount\":" + fmt.Sprintf("%.2f", weeklyAmount) +
+		",\"updated_timestamp\":\"" + time + "\"}"
 }
